a5163f95dc41515af7f09a5d8005f84a9af5e642
Javier Granero <javiergranero89@gmail.com>
2024-09-17 23:57:48 -0300

    Cambios realizados por IDE

--- TiendaApp/obj/Debug/net8.0/TiendaApp.AssemblyInfo.cs
+++ TiendaApp/obj/Debug/net8.0/TiendaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca047043c2e8a98b217e1fd7ab33c68043fbd7a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
