cedf0ac63d8e38cc6a3624554a6ec215ec2313c2
Javier Granero <javiergranero89@gmail.com>
2024-09-18 10:13:21 -0300

    Se crea una clase con métodos de prueba unitarios para la clase Producto.

---
+++ TiendaApp.tests/ProductoTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TiendaApp.Clases;
+using Xunit;
+
+namespace TiendaApp.tests
+{
+    public class ProductoTests
+    {
+        // Pruebas unitarias 
+
+        [Fact]
+        public void CrearProducto_ProductoValido_CreadoCorrectamente()      // Verifica que un producto se crea correctamente
+        {
+            // Arrange & Act
+            var producto = new Producto("Laptop", 1000.0, "Electronics");
+
+            // Assert
+            Assert.Equal("Laptop", producto.Nombre);        // Verifica que el nombre se haya asignado correctamente
+            Assert.Equal(1000.0, producto.Precio);      // Verifica que el precio se haya asignado correctamente
+            Assert.Equal("Electronics", producto.Categoria);        // Verifica que la categroría se haya asignado correctamente
+        }
+
+        [Fact]
+        public void CrearProducto_PrecioNegativo_LanzaArgumentException()       // Verifica si se lanza la excepción correspondiente cuando se intenta crear un producto con precio negativo
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new Producto("Laptop", -100.0, "Electronics"));
+        }
+
+        [Fact]
+        public void CrearProducto_NombreNulo_LanzaArgumentNullException()       // Verifica si se lanza la excepción correspondiente cuando se intenta crear un producto con el parámetro 'nombre' vacío/nulo
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new Producto(null, 1000.0, "Electronics"));
+        }
+
+        [Fact]
+        public void CrearProducto_CategoriaNula_LanzaArgumentNullException()        // Verifica si se lanza la excepción correspondiente cuando se intenta crear un producto con el parámetro 'categoría' vacío/nulo
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new Producto("Laptop", 1000.0, null));
+        }
+    }
+}
